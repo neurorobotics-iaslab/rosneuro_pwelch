3d74e342094626f80f27d9b8df1a9c7f6b3a70c6
mirovix <miroljubmihailovic98@gmail.com>
2024-05-20 22:53:59 +0200

    test: integration test and layout changes

--- test/TestPwelchNode.cpp
+++ test/TestPwelchNode.cpp
@@ -2,72 +2,70 @@
 #include <gtest/gtest.h>
 
 namespace rosneuro {
+    class TestPwelchNodeSuite : public ::testing::Test {
+        public:
+            TestPwelchNodeSuite() {}
+            ~TestPwelchNodeSuite() {}
 
-class TestPwelchNodeSuite : public ::testing::Test {
-    public:
-        TestPwelchNodeSuite() {}
-        ~TestPwelchNodeSuite() {}
+            void SetUp() override {
+                pwelch_node = new PwelchNode<double>();
+            }
 
-        void SetUp() override {
-            pwelch_node = new PwelchNode<double>();
-        }
+            void TearDown() override {
+                ros::param::del("RingBufferCfg");
+                delete pwelch_node;
+            }
 
-        void TearDown() override {
-            ros::param::del("RingBufferCfg");
-            delete pwelch_node;
-        }
+            void config(){
+                ros::NodeHandle nh;
+                XmlRpc::XmlRpcValue params, validConfig;
+                validConfig["name"] = "ringbuffer";
+                validConfig["type"] = "RingBufferFloat";
+                params["size"] = 256;
+                validConfig["params"] = params;
+                nh.setParam("RingBufferCfg", validConfig);
+            }
 
-        void config(){
-            ros::NodeHandle nh;
-            XmlRpc::XmlRpcValue params, validConfig;
-            validConfig["name"] = "ringbuffer";
-            validConfig["type"] = "RingBufferFloat";
-            params["size"] = 256;
-            validConfig["params"] = params;
-            nh.setParam("RingBufferCfg", validConfig);
-        }
+            PwelchNode<double>* pwelch_node;
+    };
 
-        PwelchNode<double>* pwelch_node;
-};
+    TEST_F(TestPwelchNodeSuite, Constructor) {
+        ASSERT_TRUE(pwelch_node->sub_.getTopic() == "/neurodata_filtered");
+        ASSERT_TRUE(pwelch_node->pub_.getTopic() == "/neurodata_psd");
+    }
 
-TEST_F(TestPwelchNodeSuite, Constructor) {
-    ASSERT_TRUE(pwelch_node->sub_.getTopic() == "/neurodata_filtered");
-    ASSERT_TRUE(pwelch_node->pub_.getTopic() == "/neurodata_psd");
-}
+    TEST_F(TestPwelchNodeSuite, Configure) {
+        config();
+        ASSERT_TRUE(pwelch_node->configure());
+        ASSERT_TRUE(pwelch_node->psd_wlength_ == 256);
+        ASSERT_TRUE(pwelch_node->psd_novl_ == 128);
+        ASSERT_TRUE(pwelch_node->psd_dolog_ == 1);
+        ASSERT_TRUE(pwelch_node->sampling_freq_ == 512);
+        ASSERT_TRUE(pwelch_node->wtype_ == 2);
+    }
 
-TEST_F(TestPwelchNodeSuite, Configure) {
-    config();
-    ASSERT_TRUE(pwelch_node->configure());
-    ASSERT_TRUE(pwelch_node->psd_wlength_ == 256);
-    ASSERT_TRUE(pwelch_node->psd_novl_ == 128);
-    ASSERT_TRUE(pwelch_node->psd_dolog_ == 1);
-    ASSERT_TRUE(pwelch_node->sampling_freq_ == 512);
-    ASSERT_TRUE(pwelch_node->wtype_ == 2);
-}
+    TEST_F(TestPwelchNodeSuite, WrongConfigure) {
+        ASSERT_FALSE(pwelch_node->configure());
+    }
 
-TEST_F(TestPwelchNodeSuite, WrongConfigure) {
-    ASSERT_FALSE(pwelch_node->configure());
-}
+    TEST_F(TestPwelchNodeSuite, OnReceivedNeuroData) {
+        config();
+        pwelch_node->configure();
 
-TEST_F(TestPwelchNodeSuite, OnReceivedNeuroData) {
-    config();
-    pwelch_node->configure();
+        rosneuro_msgs::NeuroFrame msg;
+        msg.header.stamp = ros::Time::now();
+        msg.header.frame_id = "neurodata";
+        msg.eeg.info.nsamples = 256;
+        msg.eeg.info.nchannels = 256;
+        msg.eeg.data = std::vector<float>(256*256, 2);
+        pwelch_node->on_received_neurodata(msg);
 
-    rosneuro_msgs::NeuroFrame msg;
-    msg.header.stamp = ros::Time::now();
-    msg.header.frame_id = "neurodata";
-    msg.eeg.info.nsamples = 256;
-    msg.eeg.info.nchannels = 256;
-    msg.eeg.data = std::vector<float>(256*256, 2);
-    pwelch_node->on_received_neurodata(msg);
-
-    ASSERT_EQ(pwelch_node->neuromsg_.eeg.info.nsamples, 256);
-    ASSERT_EQ(pwelch_node->neuromsg_.eeg.info.nchannels, 256);
-    ASSERT_EQ(pwelch_node->neuromsg_.eeg.data.size(), 33024);
-    ASSERT_EQ(pwelch_node->neuromsg_.exg, msg.exg);
-    ASSERT_EQ(pwelch_node->neuromsg_.tri, msg.tri);
-}
-
+        ASSERT_EQ(pwelch_node->neuromsg_.eeg.info.nsamples, 256);
+        ASSERT_EQ(pwelch_node->neuromsg_.eeg.info.nchannels, 256);
+        ASSERT_EQ(pwelch_node->neuromsg_.eeg.data.size(), 33024);
+        ASSERT_EQ(pwelch_node->neuromsg_.exg, msg.exg);
+        ASSERT_EQ(pwelch_node->neuromsg_.tri, msg.tri);
+    }
 }
 
 int main(int argc, char **argv) {
