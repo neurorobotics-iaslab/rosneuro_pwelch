4d07cfc4852b7d087cd44d3c948edd1f585614c9
mirovix <miroljubmihailovic98@gmail.com>
2024-05-20 22:53:36 +0200

    refactor: compress functions

--- include/rosneuro_pwelch/Pwelch.hpp
+++ include/rosneuro_pwelch/Pwelch.hpp
@@ -17,231 +17,196 @@
 #include "rosneuro_windows_flattop/Flattop.hpp"
 
 namespace rosneuro {
+    typedef struct {
+        unsigned int wlength;
+        unsigned int wtype;
+        unsigned int novl;
+        unsigned int nfft;
+        unsigned int fs;
+        unsigned int dolog;
+        std::vector<uint32_t> 	grid;
+    } pwelch_config;
 
-typedef struct {
-	unsigned int 		wlength;
-	unsigned int 		wtype;
-	unsigned int 		novl;
-	unsigned int 		nfft;
-	unsigned int 		fs;
-	unsigned int 	    dolog;
+    template<typename T> using DynamicMatrix = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>;
+    template <typename T>
+    class Pwelch {
+        public:
+            Pwelch(void);
+            ~Pwelch(void);
 
-	std::vector<uint32_t> 	grid;
-} pwelchconfig_t;
+            bool configure(unsigned int wlength, unsigned int wtype, unsigned novl, unsigned int fs, unsigned int dolog);
+            DynamicMatrix<T> apply(const DynamicMatrix<T>& in);
+            bool isSet(void);
+            pwelch_config config;
+            EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-template<typename T> using DynamicMatrix = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>;
-template <typename T>
-class Pwelch {
-    public:
-        Pwelch(void);
-        ~Pwelch(void);
+        private:
+            unsigned int compute_nfft(unsigned int wlength);
+            unsigned int compute_nsegments(unsigned int wlength, unsigned int novl, unsigned int nsamples);
+            void compute(const Eigen::Ref<const Eigen::VectorXd>& in, Eigen::Ref<Eigen::VectorXd> out);
+            std::vector<uint32_t> compute_grid(unsigned int wlength, unsigned int nfft, unsigned int fs);
+            void setWindow(void);
 
-		bool configure(unsigned int wlength, unsigned int wtype, unsigned novl, unsigned int fs, unsigned int dolog);
-		DynamicMatrix<T> apply(const DynamicMatrix<T>& in);
-		bool isSet(void);
-        pwelchconfig_t 	config;
-		EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+            rosneuro_msgs::NeuroFrame neuromsg_;
+            Window<T> * window_;
+            Eigen::VectorXd  wsig_;
+            Eigen::VectorXd  wpxx_;
+            fftw_plan 	plan_;
+            DynamicMatrix<T>  psd_;
+            bool 		isset_;
+            bool        psd_configured_;
 
-    private:
-        unsigned int compute_nfft(unsigned int wlength);
-		unsigned int compute_nsegments(unsigned int wlength, unsigned int novl, unsigned int nsamples);
-		void compute(const Eigen::Ref<const Eigen::VectorXd>& in, Eigen::Ref<Eigen::VectorXd> out);
-		std::vector<uint32_t> compute_grid(unsigned int wlength, unsigned int nfft, unsigned int fs);
-		rosneuro_msgs::NeuroFrame neuromsg_;
-		Window<double> * window_;
-		Eigen::VectorXd  wsig_;
-		Eigen::VectorXd  wpxx_;
-		fftw_plan 	plan_;
-		DynamicMatrix<T>  psd_;
-		bool 		isset_;
-		bool        psd_configured_;
+            FRIEND_TEST(TestPwelchSuite, Constructor);
+            FRIEND_TEST(TestPwelchSuite, Configure);
+            FRIEND_TEST(TestPwelchSuite, Isset);
+            FRIEND_TEST(TestPwelchSuite, Apply);
+    };
 
-        FRIEND_TEST(TestPwelchSuite, Constructor);
-        FRIEND_TEST(TestPwelchSuite, Configure);
-        FRIEND_TEST(TestPwelchSuite, Isset);
-        FRIEND_TEST(TestPwelchSuite, Apply);
-};
+    template <typename T>
+    Pwelch<T>::Pwelch(void){
+        this->isset_ = false;
+        this->psd_configured_ = false;
+    }
 
-template <typename T>
-Pwelch<T>::Pwelch(void){
-	this->isset_ = false;
-	this->psd_configured_ = false;
-}
+    template <typename T>
+    Pwelch<T>::~Pwelch(void) {
+        fftw_free(this->plan_);
+        free(this->window_);
+    }
 
-template <typename T>
-Pwelch<T>::~Pwelch(void) {
-	fftw_free(this->plan_);
-	free(this->window_);
-}
+    template <typename T>
+    bool Pwelch<T>::configure(unsigned int wlength, unsigned int wtype, unsigned novl, unsigned int fs, unsigned int dolog) {
+        this->config.wlength	= wlength;
+        this->config.wtype 	= wtype;
+        this->config.novl	= novl;
+        this->config.nfft 	= compute_nfft(config.wlength);
+        this->config.fs 	= fs;
+        this->config.dolog 	= dolog;
+        this->config.grid 	= compute_grid(config.wlength, this->config.nfft, config.fs);
 
-template <typename T>
-bool Pwelch<T>::configure(unsigned int wlength, unsigned int wtype, unsigned novl, unsigned int fs, unsigned int dolog) {
-	// pwelch's parameters
-	this->config.wlength	= wlength;
-	this->config.wtype 	= wtype;
-	this->config.novl	= novl;
-	this->config.nfft 	= compute_nfft(config.wlength);
-	this->config.fs 	= fs;
-	this->config.dolog 	= dolog;
-	this->config.grid 	= compute_grid(config.wlength, this->config.nfft, config.fs);	
+        this->setWindow();
 
-	// Configure window
-	switch (this->config.wtype){
-	case 1:
-		this->window_ = new Hann<double>();
-		break;
-	case 2:
-		this->window_ = new Hamming<double>();
-		break;
-	case 3:
-		this->window_ = new Blackman<double>();
-		break;
-	case 4:
-		this->window_ = new Flattop<double>();
-		break;
-	default:
-		break;
-	}
+        this->wsig_ 	= Eigen::VectorXd::Zero(this->config.wlength);
+        this->wpxx_ 	= Eigen::VectorXd::Zero(this->config.wlength);
+        this->plan_ = fftw_plan_r2r_1d(this->config.wlength, this->wsig_.data(), this->wpxx_.data(), FFTW_R2HC, FFTW_PATIENT);
+        if(this->plan_ == NULL){
+            return false;
+        }
 
-	// Initialize in and out vectors for plan creation
-	this->wsig_ 	= Eigen::VectorXd::Zero(this->config.wlength);
-	this->wpxx_ 	= Eigen::VectorXd::Zero(this->config.wlength);
+        this->psd_ = Eigen::MatrixXd::Zero(this->config.nfft, 1);
+        this->isset_ = true;
+        this->psd_configured_ = false;
+        return this->isset_;
+    }
 
-	// Create plan for fft
-	this->plan_ 	= fftw_plan_r2r_1d(this->config.wlength, this->wsig_.data(), this->wpxx_.data(), FFTW_R2HC, FFTW_PATIENT);
+    template <typename T>
+    void Pwelch<T>::setWindow(){
+        switch (this->config.wtype){
+            case 1:
+                this->window_ = new Hann<double>();
+                break;
+            case 2:
+                this->window_ = new Hamming<double>();
+                break;
+            case 3:
+                this->window_ = new Blackman<double>();
+                break;
+            case 4:
+                this->window_ = new Flattop<double>();
+                break;
+            default:
+                break;
+        }
+    }
 
-	if(this->plan_ == NULL){
-		return false;
-	}
+    template <typename T>
+    bool Pwelch<T>::isSet(void){
+        if(!this->isset_) {
+            ROS_ERROR("The pwelch is not configured");
+            throw std::runtime_error("The pwelch is not configured");
+        }
+        return this->isset_;
+    }
 
-	// Initialize internal psd
-	this->psd_ = Eigen::MatrixXd::Zero(this->config.nfft, 1);
+    template <typename T>
+    DynamicMatrix<T> Pwelch<T>::apply(const DynamicMatrix<T>& in){
+        unsigned int nchannels = in.cols();
 
-	this->isset_ = true;
+        if(!this->psd_configured_){
+            this->psd_.resize(Eigen::NoChange, in.cols());
+            this->psd_configured_ = true;
+            ROS_WARN("[Pwelch] First apply: the psd size is set");
+        }
 
-	this->psd_configured_ = false;
+        for (unsigned int i = 0; i < nchannels; i++) {
+            compute(in.col(i), this->psd_.col(i));
+        }
 
-	return this->isset_;
-}
+        if (this->config.dolog) {
+            this->psd_ = this->psd_.array().log();
+        }
 
-template <typename T>
-bool Pwelch<T>::isSet(void){
-	if(this->isset_ == false)
-		ROS_ERROR("The pwelch is not configured");
-		throw std::runtime_error("The pwelch is not configured");
+        return this->psd_;
+    }
 
-	return this->isset_;
-}
+    template <typename T>
+    void Pwelch<T>::compute(const Eigen::Ref<const Eigen::VectorXd>& in, Eigen::Ref<Eigen::VectorXd> out) {
+        unsigned int nsamples = in.size();
+        unsigned int wlength = this->config.wlength;
+        unsigned int novl = this->config.novl;
+        unsigned int nfft = this->config.nfft;
+        unsigned int fs = this->config.fs;
 
-template <typename T>
-DynamicMatrix<T> Pwelch<T>::apply(const DynamicMatrix<T>& in){
-	unsigned int nchannels = in.cols();
-	
-	if(!this->psd_configured_){
-		this->psd_.resize(Eigen::NoChange, in.cols());
-		this->psd_configured_ = true;
-		ROS_WARN("[Pwelch] First apply: the psd size is set");
-	}
+        int nsegments = compute_nsegments(wlength, novl, nsamples);
+        Eigen::VectorXd power_spectral_density	= Eigen::VectorXd::Zero(nfft);
 
-	for (unsigned int i = 0; i < nchannels; i++) {
-		compute(in.col(i), this->psd_.col(i));
-	}
+        for (unsigned int segId = 0; segId < nsegments; ++segId) {
+            this->wsig_ = this->window_->apply(in.segment(segId*(wlength - novl), wlength));
+            fftw_execute_r2r(this->plan_, this->wsig_.data(), this->wpxx_.data());
 
-	// log norm
-	if (this->config.dolog) {
-		this->psd_ = this->psd_.array().log();
-	}
+            // out spans from 0 to wLength/2 (NFFT = wLength/2 + 1)
+            power_spectral_density(0) += pow(this->wpxx_(0), 2);
+            power_spectral_density(wlength/2) += pow(this->wpxx_(wlength/2), 2);
 
-	return this->psd_;
-}
+            //	out(k) 	 = wpxx(k)^2 + wpxx(n-k)^2, k = 1 : (n/2 - 1) [Real and imagery part]
+            power_spectral_density.segment(1, wlength/2 - 1).array() += this->wpxx_.segment(1, wlength/2 - 1).array().pow(2)
+                                       + this->wpxx_.segment(wlength/2 + 1, wlength/2 - 1).reverse().array().pow(2);
 
-template <typename T>
-void Pwelch<T>::compute(const Eigen::Ref<const Eigen::VectorXd>& in, Eigen::Ref<Eigen::VectorXd> out) {
-	
-	unsigned int nsamples   = in.size();	
-	unsigned int wlength	= this->config.wlength;
-    unsigned int novl       = this->config.novl;
-	unsigned int nfft    	= this->config.nfft;
-	unsigned int fs      	= this->config.fs;
-	
-	unsigned int nsegments  = compute_nsegments(wlength, novl, nsamples);
-	double pxxnorm;
+        }
 
-	Eigen::VectorXd  wsegm 	= Eigen::VectorXd::Zero(wlength);
-	Eigen::VectorXd  pxx		= Eigen::VectorXd::Zero(nfft);
+        double window_norm = this->window_->GetWindowNorm();
+        double power_spectral_density_factor = (nsegments * window_norm * fs * wlength)/2.0;
 
-	unsigned int segId 	= 0;
-	unsigned int sstart; 
+        power_spectral_density(0) /= (2.0 * power_spectral_density_factor);
+        power_spectral_density(wlength / 2) /= (2.0 * power_spectral_density_factor);
+        power_spectral_density.segment(1, wlength / 2 - 1).array() /= power_spectral_density_factor;
 
-	while(segId < nsegments) {
+        out = power_spectral_density;
+    }
 
-		sstart  = segId*(wlength - novl);
-		wsegm 	= in.segment(sstart, wlength); 
-		this->wsig_ = this->window_->apply(wsegm);
-		fftw_execute_r2r(this->plan_, this->wsig_.data(), this->wpxx_.data());
-		
-		// out spans from 0 to wLength/2 (NFFT = wLength/2 + 1)
-		// ex. 	wLength  = n = 256
-		//	NFFT          = 129 (0:1:128)
-		//
-		//	out(0) 	 = wpxx(0)^2 	[Only real part - Half complex vector]
-		//	out(n/2) = wpxx(n/2)^2	[Only real part - wlenght is even - Half complex vector]
-		//	out(k) 	 = wpxx(k)^2 + wpxx(n-k)^2, k = 1 : (n/2 - 1) [Real and imagery part]
+    template <typename T>
+    unsigned int Pwelch<T>::compute_nsegments(unsigned int wlength, unsigned int novl, unsigned int nsamples) {
 
-		pxx(0) 		+= pow(this->wpxx_(0), 2);
-		pxx(wlength/2) 	+= pow(this->wpxx_(wlength/2), 2);
-		
-		pxx.segment(1, wlength/2 - 1).array() += this->wpxx_.segment(1, wlength/2 - 1).array().pow(2) 
-					               + this->wpxx_.segment(wlength/2 + 1, wlength/2 - 1).reverse().array().pow(2);
-		
-		segId++;
-	}
-	
-	/* NORMALIZATION FACTOR */
-	double wnorm		= this->window_->GetWindowNorm();
-	pxxnorm = (nsegments * wnorm * fs * wlength)/2.0;
+        if (wlength == novl)
+            novl = wlength - 1;
 
-	pxx(0) = pxx(0) / (2.0 * pxxnorm);
-	pxx(wlength/2) = pxx(wlength/2) / (2.0 * pxxnorm);
-	pxx.segment(1, wlength/2 - 1) = pxx.segment(1, wlength/2 - 1) / pxxnorm;
+        return floor((nsamples - novl) / (wlength - novl));
+    }
 
-	out = pxx;
-}
+    template <typename T>
+    unsigned int Pwelch<T>::compute_nfft(unsigned int wlength) {
+        return floor(wlength/2) + 1;
+    }
 
-template <typename T>
-unsigned int Pwelch<T>::compute_nsegments(unsigned int wlength, unsigned int novl, unsigned int nsamples) {
-	
-	if (wlength == novl)
-		novl = wlength - 1;
-
-	return floor((nsamples - novl) / (wlength - novl));
-}
-
-template <typename T>
-unsigned int Pwelch<T>::compute_nfft(unsigned int wlength) {
-	return floor(wlength/2) + 1;
-}
-
-template <typename T>
-std::vector<uint32_t> Pwelch<T>::compute_grid(unsigned int wlength, unsigned int nfft, unsigned int fs) {
-
-	Eigen::VectorXi grid;
-	std::vector<uint32_t> vgrid;
-	unsigned int i;
-
-	// Edited by L.Tonin  <luca.tonin@epfl.ch> on 22/07/19 10:16:46
-	// Need to re-cast to int otherwise compilation error
-	
-	grid = Eigen::VectorXi::LinSpaced(nfft, 0, nfft - 1) * (int)std::floor(fs/wlength);
-	//grid = Eigen::VectorXi::LinSpaced(nfft, 0, nfft - 1) * floor(fs/wlength);
-
-	for (i=0; i< grid.size(); i++)
-		vgrid.push_back(grid(i));
-
-	return vgrid;
-
-}
-
+    template <typename T>
+    std::vector<uint32_t> Pwelch<T>::compute_grid(unsigned int wlength, unsigned int nfft, unsigned int fs) {
+        std::vector<uint32_t> vgrid;
+        Eigen::VectorXi grid = Eigen::VectorXi::LinSpaced(nfft, 0, nfft - 1) * (int)std::floor(fs/wlength);
+        for (unsigned int i=0; i< grid.size(); i++) {
+            vgrid.push_back(grid(i));
+        }
+        return vgrid;
+    }
 }
 
 #endif
--- include/rosneuro_pwelch/PwelchNode.h
+++ include/rosneuro_pwelch/PwelchNode.h
@@ -8,145 +8,117 @@
 #include <gtest/gtest_prod.h>
 
 namespace rosneuro{
+    template <typename T>
+    class PwelchNode{
+        public:
+            PwelchNode(void);
+            ~PwelchNode(void);
 
-template <typename T>
-class PwelchNode{
-    public:
-        PwelchNode(void);
-        ~PwelchNode(void);
+            bool configure(void);
+            void run(void);
 
-        bool configure(void);
-        void run(void);
+        private:
+            void on_received_neurodata(const rosneuro_msgs::NeuroFrame& msg);
 
-	private:
-		void on_received_neurodata(const rosneuro_msgs::NeuroFrame& msg);
+            ros::NodeHandle nh_;
+            ros::NodeHandle p_nh_;
+            ros::Subscriber sub_;
+            ros::Publisher  pub_;
+            unsigned int psd_wlength_;
+            unsigned int psd_novl_;
+            unsigned int psd_dolog_;
+            unsigned int sampling_freq_;
+            unsigned int wtype_;
 
-	private:
-		ros::NodeHandle nh_;
-		ros::NodeHandle p_nh_;
-		ros::Subscriber sub_;
-		ros::Publisher  pub_;
-        unsigned int                    psd_wlength_;
-        unsigned int                    psd_novl_;
-	    unsigned int                    psd_dolog_; 
-        unsigned int                    sampling_freq_;
-        unsigned int                    wtype_; 
+            Pwelch<T> * pwelch_;
+            Buffer<T> * buffer_;
+            rosneuro_msgs::NeuroFrame neuromsg_;
 
-		Pwelch<T> * pwelch_;
-		Buffer<T> * buffer_;
-		rosneuro_msgs::NeuroFrame neuromsg_;
+            FRIEND_TEST(TestPwelchNodeSuite, Constructor);
+            FRIEND_TEST(TestPwelchNodeSuite, Configure);
+            FRIEND_TEST(TestPwelchNodeSuite, WrongConfigure);
+            FRIEND_TEST(TestPwelchNodeSuite, OnReceivedNeuroData);
+    };
 
-        FRIEND_TEST(TestPwelchNodeSuite, Constructor);
-        FRIEND_TEST(TestPwelchNodeSuite, Configure);
-        FRIEND_TEST(TestPwelchNodeSuite, WrongConfigure);
-        FRIEND_TEST(TestPwelchNodeSuite, OnReceivedNeuroData);
-};
-
-template <typename T>
-PwelchNode<T>::PwelchNode(void) : p_nh_("~") {
-    this->sub_ = this->nh_.subscribe("/neurodata_filtered", 100, &PwelchNode<T>::on_received_neurodata, this);
-    this->pub_ = this->nh_.advertise<rosneuro_msgs::NeuroFrame>("/neurodata_psd", 1); // may be is better with p_nh_
-}
-template <typename T>
-PwelchNode<T>::~PwelchNode(void){}
-
-template <typename T>
-bool PwelchNode<T>::configure(void){
-    this->p_nh_.param<int>("psd_wlength", (int&) this->psd_wlength_, 256);
-	this->p_nh_.param<int>("psd_novl", (int&) this->psd_novl_, 128);
-	this->p_nh_.param<int>("psd_dolog", (int&) this->psd_dolog_, 1); 
-    this->p_nh_.param<int>("sampling_freq", (int&) this->sampling_freq_, 512);
-    this->p_nh_.param<int>("wtype", (int&) this->wtype_, 2); // 2 is hamming
-
-	this->buffer_ = new rosneuro::RingBuffer<double>();
-	if(buffer_->configure("RingBufferCfg") == false){ 
-		ROS_ERROR("[%s] RingBuffer configuration failed", this->buffer_->name().c_str());
-        return false;
-	}
-	ROS_INFO("[RingBuffer] RingBuffer has been configured");
-
-	this->pwelch_ = new rosneuro::Pwelch<double>();
-    if(this->pwelch_->configure(this->psd_wlength_, this->wtype_, this->psd_novl_, this->sampling_freq_, this->psd_dolog_) == false){
-        ROS_ERROR("[Pwelch] Pwelch configuration failed");
-        return false;
+    template <typename T>
+    PwelchNode<T>::PwelchNode(void) : p_nh_("~") {
+        this->sub_ = this->nh_.subscribe("/neurodata_filtered", 100, &PwelchNode<T>::on_received_neurodata, this);
+        this->pub_ = this->nh_.advertise<rosneuro_msgs::NeuroFrame>("/neurodata_psd", 1);
     }
 
-    ROS_INFO("[Pwelch] Pwelch has been configured");
-    return true;
-}   
-
-template <typename T>
-void PwelchNode<T>::run(void){
-
-	ros::Rate r(512);
-
-	while(ros::ok()) {
-		ros::spinOnce();
-		r.sleep();
-	}
-}
-
-template <typename T>
-void PwelchNode<T>::on_received_neurodata(const rosneuro_msgs::NeuroFrame& msg) {
-
-	// Getting information from the message
-	float* ptr_in;
-	rosneuro::DynamicMatrix<float> data_in;
-	rosneuro::DynamicMatrix<double> data_in_T;
-	rosneuro::DynamicMatrix<double> data_out_T;
-	rosneuro::DynamicMatrix<double> data_out;
-
-	unsigned int nsamples   = msg.eeg.info.nsamples;
-	unsigned int nchannels  = msg.eeg.info.nchannels;
-
-	// Getting pointer to the input data message
-	ptr_in = const_cast<float*>(msg.eeg.data.data());
-
-	// Re-map raw pointer to eigen matrix.
-	// Remember input data is stored as [channels x samples]
-	data_in = Eigen::Map<rosneuro::DynamicMatrix<float>>(ptr_in, nchannels, nsamples);
-
-    // Transpose input data to [samples x channels]
-	data_in_T = data_in.transpose().cast<double>();
-	
-	// Add data to the buffer
-	this->buffer_->add(data_in_T);
-
-	// Perform pwelch only if buffer full
-	if(this->buffer_->isfull()){
-		
-		// Take data from the buffer [nsamples x nchannels]
-		rosneuro::DynamicMatrix<double> buffer_data = this->buffer_->get();
-		int nchannels = buffer_data.cols();
-
-		// Apply pwelch [frequency x nchannels]
-    	data_out_T = this->pwelch_->apply(buffer_data);
-		unsigned int nfreqs = data_out_T.rows();
-
-		// Transpose again data out to [nchannels x frequency]
-		data_out = data_out_T.transpose();
-       
-	   	// Re-map eigen matrix to std::vector
-	   	std::vector<float> neuroeeg(nfreqs * nchannels);
-	   	Eigen::Map<rosneuro::DynamicMatrix<float>>(neuroeeg.data(), nchannels, nfreqs) = data_out.cast<float>();
-	   	
-	   	// Setting message
-	   	this->neuromsg_.header.stamp = ros::Time::now();
-		this->neuromsg_.eeg.data = neuroeeg;
-		this->neuromsg_.sr  = msg.sr;
-		this->neuromsg_.exg = msg.exg;
-		this->neuromsg_.tri = msg.tri;
-		this->neuromsg_.eeg.info = msg.eeg.info;
-    
-	   	// Publishing the message
-	   	this->pub_.publish(this->neuromsg_);
-
-	}else{
-	   	ROS_INFO("[%s] Buffer is not full", this->buffer_->name().c_str());
+    template <typename T>
+    PwelchNode<T>::~PwelchNode(void){
     }
-	std::cout << "seq added: " << msg.header.seq << std::endl;
-}
 
+    template <typename T>
+    bool PwelchNode<T>::configure(void){
+        this->p_nh_.param<int>("psd_wlength", (int&) this->psd_wlength_, 256);
+        this->p_nh_.param<int>("psd_novl", (int&) this->psd_novl_, 128);
+        this->p_nh_.param<int>("psd_dolog", (int&) this->psd_dolog_, 1);
+        this->p_nh_.param<int>("sampling_freq", (int&) this->sampling_freq_, 512);
+        this->p_nh_.param<int>("wtype", (int&) this->wtype_, 2);
+
+        this->buffer_ = new RingBuffer<double>();
+        if(buffer_->configure("RingBufferCfg") == false){
+            ROS_ERROR("[%s] RingBuffer configuration failed", this->buffer_->name().c_str());
+            return false;
+        }
+        ROS_INFO("[RingBuffer] RingBuffer has been configured");
+
+        this->pwelch_ = new rosneuro::Pwelch<double>();
+        if(this->pwelch_->configure(this->psd_wlength_, this->wtype_, this->psd_novl_, this->sampling_freq_, this->psd_dolog_) == false){
+            ROS_ERROR("[Pwelch] Pwelch configuration failed");
+            return false;
+        }
+
+        ROS_INFO("[Pwelch] Pwelch has been configured");
+        return true;
+    }
+
+    template <typename T>
+    void PwelchNode<T>::run(void){
+        ros::Rate r(512);
+        while(ros::ok()) {
+            ros::spinOnce();
+            r.sleep();
+        }
+    }
+
+    template <typename T>
+    void PwelchNode<T>::on_received_neurodata(const rosneuro_msgs::NeuroFrame& msg) {
+        unsigned int nsamples   = msg.eeg.info.nsamples;
+        unsigned int nchannels  = msg.eeg.info.nchannels;
+        float* ptr_in = const_cast<float*>(msg.eeg.data.data());
+
+        DynamicMatrix<float> data_in = Eigen::Map<rosneuro::DynamicMatrix<float>>(ptr_in, nchannels, nsamples);
+        DynamicMatrix<double> data_in_T = data_in.transpose().cast<double>();
+        this->buffer_->add(data_in_T);
+
+        if(this->buffer_->isfull()){
+            DynamicMatrix<double> buffer_data = this->buffer_->get();
+            int nchannels = buffer_data.cols();
+
+            DynamicMatrix<double> data_out_T = this->pwelch_->apply(buffer_data);
+            unsigned int nfreqs = data_out_T.rows();
+
+            DynamicMatrix<double> data_out = data_out_T.transpose();
+            std::vector<float> neuroeeg(nfreqs * nchannels);
+            Eigen::Map<rosneuro::DynamicMatrix<float>>(neuroeeg.data(), nchannels, nfreqs) = data_out.cast<float>();
+
+            this->neuromsg_.header.stamp = ros::Time::now();
+            this->neuromsg_.eeg.data = neuroeeg;
+            this->neuromsg_.sr  = msg.sr;
+            this->neuromsg_.exg = msg.exg;
+            this->neuromsg_.tri = msg.tri;
+            this->neuromsg_.eeg.info = msg.eeg.info;
+
+            this->pub_.publish(this->neuromsg_);
+
+        }else{
+            ROS_INFO("[%s] Buffer is not full", this->buffer_->name().c_str());
+        }
+        std::cout << "seq added: " << msg.header.seq << std::endl;
+    }
 }
 
 #endif
